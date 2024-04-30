28758bd70ac627100b0340dbf07eca264f30554e
importimport <73524267+importimport@users.noreply.github.com>
2024-04-30 14:48:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import jieba
-from wordcloud import wordcloud
+
 import os
 import re
 import zipfile
